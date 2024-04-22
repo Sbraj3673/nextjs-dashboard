4f4dd623f19760b4687386283250a34abfaf170b
Sbraj3673 <sbraj3673@gmail.com>
2024-04-22 16:24:13 +0530

    first commit

---
+++ app/dash/page.tsx
@@ -0,0 +1,11 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
+
+
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
